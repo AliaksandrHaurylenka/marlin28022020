9bcaa6038d1af26fc66d10628e8a0e4e466e56ba
AliaksandrHaurylenka <goric0312@mail.ru>
2020-03-18 10:59:13 +0300

    17-18

---
+++ 17-18/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once('init.php');
+
+// var_dump(Config::get('session.user_session'));
+
+$user = new User;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>17-18</title>
+</head>
+<body class="container mt-5">
+  <?php if($user->isLoggedIn()): ?>
+    <h1>Привет, <?= $user->data()->name; ?></h1>
+    <button class="btn btn-defaul"><a href='logout.php'>Logout</a></button>
+  <?php else: ?>
+    <button class="btn btn-success"><a href="register.php" class="text-white">Регистрация</a></button>
+    <button class="btn btn-primary"><a href="login.php" class="text-white">Войти</a></button>
+  <?php endif; ?> 
+</body>
+</html>
---
+++ 17-18/login.php
@@ -0,0 +1,63 @@
+<?php
+require_once('init.php');
+
+if(Input::exists()){
+  if(Token::check(Input::get('token'))){
+    $validate = new Validate();
+
+    $validation = $validate -> check($_POST, [
+      'email' => [
+        'required' => true,
+        'email' => true,
+      ],
+  
+      'password' => [
+        'required' => true,
+      ],
+    ]);
+  
+    if($validation->passed()){
+      $user = new User;
+      $login = $user->login(strip_tags(trim(Input::get('email'))), strip_tags(trim(Input::get('password'))));
+      if($login){
+        // echo 'login OK';
+        // var_dump($login); die;
+        Redirect::to('/17-18');
+      }else {
+        echo "login NO";
+      }
+    } else {
+      foreach($validation->errors() as $error){
+        echo $error . '<br>';
+      }
+    }
+  }
+  
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body class="container mt-5">
+  <?= Session::flash('success'); ?>
+  <form action="" method="post">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="text" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="<?= Input::get('email') ?>">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <input type="hidden" name="token" value="<?= Token::generate(); ?>">
+    <button type="submit" class="btn btn-primary">Войти</button>
+    <button type="submit" class="btn btn-success"><a href="/17-18" class="text-white">На главную</a></button>
+  </form>
+</body>
+</html>
